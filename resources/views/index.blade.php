15fa6f4405c82dc96a5f48f796e77a990332c0c4
Giovanni Mauro <giovi.mauro03@gmail.com>
2024-01-24 16:25:46 +0100

    file inizialization

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container-fluid vh-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12 text-center">
+                <h1>Benvenuti a tutti</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
